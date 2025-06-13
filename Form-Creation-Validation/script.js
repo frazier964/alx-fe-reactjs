640a29dcef967a608b74bc2dae9f1365a1ee37d1
frazier964 <hillarybella2@gmail.com>
2025-06-13 18:34:49 +0300

    second

--- Form-Creation-Validation/script.js
+++ Form-Creation-Validation/script.js
@@ -12,19 +12,19 @@ document.addEventListener("DOMContentLoaded", function () {
         let isValid = true;
         let messages = [];
 
-        
+        // Username Validation
         if (username.length < 3) {
             isValid = false;
             messages.push("Username must be at least 3 characters long.");
         }
 
-        
+        // Email Validation
         if (!email.includes("@") || !email.includes(".")) {
             isValid = false;
             messages.push("Email must contain '@' and '.' characters.");
         }
 
-        
+      
         if (password.length < 8) {
             isValid = false;
             messages.push("Password must be at least 8 characters long.");
