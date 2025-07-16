17d7a85817c8a906b40cf7465cfeb024be71b3be
frazier964 <hillarybella2@gmail.com>
2025-07-16 22:41:38 +0300

    first

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
+import WelcomeMessage from './components/WelcomeMessage'; 
 
 function App() {
   return (
     <div>
       <Header />
       <MainContent />
+      <WelcomeMessage /> 
       <Footer />
     </div>
   );
