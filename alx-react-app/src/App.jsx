3cf8dfdef76bf87d6cb8088666bc034922e18d98
frazier964 <hillarybella2@gmail.com>
2025-07-16 03:19:42 +0300

    question

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -25,9 +25,7 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-        <div>
           <WelcomeMessage/>
-        </div>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
