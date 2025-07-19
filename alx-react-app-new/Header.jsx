3fe4f1da9fd3c6c19aa2a83a20c649b3cb824bb8
frazier964 <hillarybella2@gmail.com>
2025-07-19 15:49:18 +0300

    styling

--- alx-react-app-new/Header.jsx
+++ alx-react-app-new/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 
 function Header() {
+  const headerStyle = {
+    backgroundColor: 'navy',
+    color: 'white',
+    textAlign: 'center',
+    padding: '30px 0',
+    fontFamily: 'Arial, sans-serif',
+    borderBottom: '5px solid #00ccff',
+    boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.3)',
+  };
+
+  const titleStyle = {
+    margin: 0,
+    fontSize: '2.5rem',
+    letterSpacing: '1px',
+  };
+
   return (
-    <header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: '20px' }}>
-      <h1>My Favorite Cities</h1>
+    <header style={headerStyle}>
+      <h1 style={titleStyle}>My Favorite Cities</h1>
     </header>
   );
 }
