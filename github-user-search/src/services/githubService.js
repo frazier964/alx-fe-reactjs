c23c4f9fee388e7875800ed167838975775dfa34
frazier964 <hillarybella2@gmail.com>
2025-08-01 22:34:13 +0300

    useroneservicedoneee

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = import.meta.env.VITE_GITHUB_API_URL;
+"https://api.github.com"
 
 export const fetchUserData = async (username) => {
   const response = await axios.get(`${BASE_URL}/users/${username}`);
