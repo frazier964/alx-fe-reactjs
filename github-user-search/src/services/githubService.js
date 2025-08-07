f420cd82d68b8ccd96bb531a59d6295e6b499240
frazier964 <hillarybella2@gmail.com>
2025-08-07 23:04:03 +0300

    tailwindddww

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,37 +1,11 @@
 import axios from 'axios';
 
 
-const BASE_URL = import.meta.env.VITE_GITHUB_API_URL || 'https://api.github.com/search/users';
+const BASE_URL = import.meta.env.VITE_GITHUB_API_URL || 'https://api.github.com/search/users?q';
 
-/**
- * Fetch detailed data for a single GitHub user by username
- */
-export const fetchUserData = async (username) => {
-  const response = await axios.get(`${BASE_URL}/users/${username}`);
-  return response.data;
-};
 
-/**
- * Advanced search for GitHub users
- * @param {Object} params - Search parameters
- * @param {string} params.username - GitHub username or keyword
- * @param {string} [params.location] - User location
- * @param {number} [params.minRepos] - Minimum number of public repositories
- */
-// export const searchUsers = async ({ username, location, minRepos }) => {
-//   let query = '';
 
-//   if (username) query += `${username} in:login`;
-//   if (location) query += ` location:${location}`;
-//   if (minRepos) query += ` repos:>=${minRepos}`;
 
-//   if (!query.trim()) throw new Error('Please provide at least one search criteria.');
-
-//   const response = await axios.get(`${BASE_URL}/search/users?q=${encodeURIComponent(query)}`);
-//   console.log('Search query:', query);
-//   console.log('API response:', response.data);
-//   return response.data;
-// };
 
 
 export async function searchUsers({ username, location, minRepos }) {
@@ -44,7 +18,8 @@ export async function searchUsers({ username, location, minRepos }) {
 
     console.log('Query:', query); // Optional: for debugging
 
-    const response = await axios.get(BASE_URL, {
+    const fullUrl =`${BASE_URL}=${encodeURIComponent(query)}`;
+    const response = await axios.get(fullUrl, {
       params: {
         q: query,
       },
