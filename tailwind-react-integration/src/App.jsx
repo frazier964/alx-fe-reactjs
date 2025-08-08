860f9ee10f655160341bb706c14c3213c1647b12
frazier964 <hillarybella2@gmail.com>
2025-08-08 18:41:19 +0300

    inntegration

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,9 +1,9 @@
+import UserProfile from "./components/UserProfile";
+
 export default function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-blue-500">
-      <h1 className="text-4xl font-bold text-white">
-        Tailwind CSS is working! 🎉
-      </h1>
+      <UserProfile />
     </div>
-  )
+  );
 }
