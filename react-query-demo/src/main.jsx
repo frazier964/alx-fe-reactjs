fd511642a55e6eb2a49bb1699602fd3e19848971
frazier964 <hillarybella2@gmail.com>
2025-08-21 12:12:29 +0300

    mainnreactt

--- react-query-demo/src/main.jsx
+++ react-query-demo/src/main.jsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 import App from './App.jsx'
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
-// Create a client instance
+// Create a QueryClient instance
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
