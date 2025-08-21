d4d38257addad3af0ca213c39defa2330daffca8
frazier964 <hillarybella2@gmail.com>
2025-08-21 12:10:04 +0300

    mainnreact

---
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -0,0 +1,28 @@
+import { useQuery } from '@tanstack/react-query'
+
+function PostsComponent() {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['posts'],
+    queryFn: async () => {
+      const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+      if (!res.ok) throw new Error('Network response was not ok')
+      return res.json()
+    },
+  })
+
+  if (isLoading) return <p>Loading...</p>
+  if (error) return <p>Error: {error.message}</p>
+
+  return (
+    <div>
+      <h2>Posts</h2>
+      <ul>
+        {data.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default PostsComponent
