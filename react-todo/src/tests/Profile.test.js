ecbc94eaca3e7519bde1a2d4d78c0ed9fad13e55
frazier964 <hillarybella2@gmail.com>
2025-08-22 02:19:25 +0300

    todoolist

--- react-todo/src/tests/Profile.test.js
+++ react-todo/src/tests/Profile.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import Profile from "../components/Profile";
 
-test("renders Profile component with name", () => {
-  render(<Profile name="Hillary" />);
-  expect(screen.getByText(/Profile/i)).toBeInTheDocument();
-  expect(screen.getByText(/Hillary/i)).toBeInTheDocument();
+test("renders profile heading", () => {
+  render(<Profile />);
+  const heading = screen.getByText(/profile/i);
+  expect(heading).toBeInTheDocument();
 });
