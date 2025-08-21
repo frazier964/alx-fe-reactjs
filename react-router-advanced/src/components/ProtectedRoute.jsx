959f298e2ae46008f3d12c32ce4f74381f778961
frazier964 <hillarybella2@gmail.com>
2025-08-21 21:26:21 +0300

    routterradvanceddd

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 
 // Fake auth state (later we can improve with useState or context)
 const isAuthenticated = false;
-
+useAuth
 export default function ProtectedRoute() {
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
 }
